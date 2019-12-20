8e7dc04a074b27213fde1b6209422f9fe0d06009
Anton Podchuvalov <podchuvalovvl@gmail.com>
2019-12-21 01:27:59 +1000

    Fix multiple print of answer in WGC. Refactor code

--- wolf_goat_cabbage.pl
+++ wolf_goat_cabbage.pl
@@ -4,57 +4,61 @@ member_(X, [_ | T]) :- member_(X, T).
 select_(X, [X | T], T).
 select_(X, [H | T], [H | T1]) :- select_(X, T, T1).
 
-solve_dfs(S, _, []) :- final_state(S).
-solve_dfs(S, H, [M | Ms]) :-
-    move(S, M),
-    update(S, M, S1),
-    legal(S1),
-    not(member_(S1, H)),
-    solve_dfs(S1, [S1 | H], Ms).
-
-test_dfs(Name, Ms) :-
-    initial_state(Name, S),
-    solve_dfs(S, [S], Ms).
-
-initial_state(wgc, wgc(l, [w, g, c], [])).
-
-move(wgc(l, L, _), M) :- member_(M, L).
-move(wgc(r, _, R), M) :- member_(M, R).
-move(wgc(_, _, _), alone).
-
-update(wgc(C, L, R), M, wgc(C1, L1, R1)) :-
-    update_coast(C, C1),
-    update_items(M, C1, L, R, L1, R1).
-
-update_coast(l, r).
-update_coast(r, l).
-
-update_items(alone, _, L, R, L, R).
-update_items(M, l, L, R, L1, R1) :-
-    select_(M, R, R1),
-    insert_(M, L, L1).
-update_items(M, r, L, R, L1, R1) :-
-    select_(M, L, L1),
-    insert_(M, R, R1).
-
-precedes(w, _).
-precedes(_, c).
-
 insert_(X, [], [X]).
 insert_(X, [H | T], [X, H | T]) :- precedes(X, H).
 insert_(X, [H | T], [H | T1]) :-
     precedes(H, X),
     insert_(X, T, T1).
 
-legal(wgc(l, _, R)) :- not(illegal(R)).
-legal(wgc(r, L, _)) :- not(illegal(L)).
+precedes(wolf, [goat, cabbage]).
+precedes(wolf, goat).
+precedes(wolf, cabbage).
+precedes(wolf, []).
+precedes(goat, cabbage).
+precedes(goat, []).
 
-illegal(C) :- member_(w, C), member_(g, C).
-illegal(C) :- member_(g, C), member_(c, C).
+illegal(Coast) :- member_(wolf, Coast), member_(goat, Coast).
+illegal(Coast) :- member_(goat, Coast), member_(cabbage, Coast).
 
-final_state(wgc(r, [], [w, g, c])).
+legal(state(left, _, Right)) :- not(illegal(Right)).
+legal(state(right, Left, _)) :- not(illegal(Left)).
 
-solve_puzzle(X):-test_dfs(wgc, X).
+update_items(alone, _, Left, Right, Left, Right).
+update_items(Item, left, Left, Right, NewLeft, NewRight) :-
+    select_(Item, Right, NewRight),
+    insert_(Item, Left, NewLeft).
+update_items(Item, right, Left, Right, NewLeft, NewRight) :-
+    select_(Item, Left, NewLeft),
+    insert_(Item, Right, NewRight).
+
+update_coast(left, right).
+update_coast(right, left).
+
+update(state(Coast, Left, Right), Item, state(NewCoast, NewLeft, NewRight)) :-
+    update_coast(Coast, NewCoast),
+    update_items(Item, NewCoast, Left, Right, NewLeft, NewRight).
+
+move(state(left, Left, _), Item) :- member_(Item, Left).
+move(state(right, _, Right), Item) :- member_(Item, Right).
+move(state(_, _, _), alone).
+
+final_state(state(right, [], [wolf, goat, cabbage])).
+
+solve_dfs(State, _, []) :- final_state(State).
+solve_dfs(State, History, [Move | Moves]) :-
+    move(State, Move),
+    update(State, Move, NewState),
+    legal(NewState),
+    not(member_(NewState, History)),
+    solve_dfs(NewState, [NewState | History], Moves).
+
+initial_state(state, state(left, [wolf, goat, cabbage], [])).
+
+test_dfs(Name, Moves) :-
+    initial_state(Name, State),
+    solve_dfs(State, [State], Moves).
+
+solve_puzzle(X):-test_dfs(state, X).
 
 
 
